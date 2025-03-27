6ccea5049d653a0b315e9977d8ee9be19db36622
Übel Brandt <ubelbrandt@gmail.com>
2025-03-27 04:41:25 -0400

    Scale canvas instead of body and custom cursor works on whole viewport.

--- 2004scale.js
+++ 2004scale.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         2004scale
 // @namespace    http://tampermonkey.net/
-// @version      2025-03-27
+// @version      2025-03-28
 // @description  Improves the browser experience of 2004scape.
 // @author       Ubel
 // @include      *lostcity.rs*
@@ -16,13 +16,15 @@
     // hide controls
     document.getElementById('controls').style.display = 'none';
 
+    // set html and body to fill screen
+    GM_addStyle("html, body { width: 100%; height: 100%; }");
+
     // set default size cursor
     GM_addStyle("* { cursor: url(https://github.com/UbelBrandt/2004scale/blob/main/cursor1x.gif?raw=true), auto; }");
 
     // scaling settings
-    GM_addStyle("body { image-rendering: crisp-edges !important; transform-origin: center top; transform: scale(1); }");
-    GM_addStyle("canvas { image-rendering: crisp-edges !important; }");
-    GM_addStyle("@media (width >= 1578px) and (height >= 1064px) { body { transform: scale(2); } * { cursor: url(https://github.com/UbelBrandt/2004scale/blob/main/cursor2x.gif?raw=true), auto; } }");
-    GM_addStyle("@media (width >= 2367px) and (height >= 1596px) { body { transform: scale(3); } * { cursor: url(https://github.com/UbelBrandt/2004scale/blob/main/cursor3x.gif?raw=true), auto; } }");
-    GM_addStyle("@media (width >= 3156px) and (height >= 2128px) { body { transform: scale(4); } * { cursor: url(https://github.com/UbelBrandt/2004scale/blob/main/cursor4x.gif?raw=true), auto; } }");
+    GM_addStyle("canvas { image-rendering: crisp-edges !important; transform-origin: center top; transform: scale(1); }");
+    GM_addStyle("@media (width >= 1578px) and (height >= 1064px) { canvas { transform: scale(2); } * { cursor: url(https://github.com/UbelBrandt/2004scale/blob/main/cursor2x.gif?raw=true), auto; } }");
+    GM_addStyle("@media (width >= 2367px) and (height >= 1596px) { canvas { transform: scale(3); } * { cursor: url(https://github.com/UbelBrandt/2004scale/blob/main/cursor3x.gif?raw=true), auto; } }");
+    GM_addStyle("@media (width >= 3156px) and (height >= 2128px) { canvas { transform: scale(4); } * { cursor: url(https://github.com/UbelBrandt/2004scale/blob/main/cursor4x.gif?raw=true), auto; } }");
 })();
